b18f36730890d8956e784ab29711c8d9d65e375c
Lance Fallon <lfallon@catalystitservices.com>
2015-03-09 15:36:46 -0400

    stage2 complete

--- src/main/java/com/flywaychallenge/app/Driver.java
+++ src/main/java/com/flywaychallenge/app/Driver.java
@@ -2,8 +2,10 @@ package com.flywaychallenge.app;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.FlywayException;
+import org.springframework.jdbc.BadSqlGrammarException;
 
 import com.flywaychallenge.models.Employee;
 
@@ -11,6 +13,8 @@ import com.flywaychallenge.models.Employee;
 
 public class Driver {
 
+	private static final Logger logger = Logger.getLogger(Driver.class);
+	
 	public static void main(String[] args) {
 		//Create instance of flyway
 		FlywayHelper flywayHelper = new FlywayHelper();
@@ -18,10 +22,10 @@ public class Driver {
 		
 		//Migrate db using flyway object
 		try{
-			// TODO
+
 		}
 		catch(FlywayException e){
-			System.out.println("Unable to migrate: " + e.getMessage());
+			logger.error("Unable to migrate: " + e.getMessage());
 		}
 		
 		//Get flyway migration status
@@ -29,13 +33,18 @@ public class Driver {
 		
 		//Get Data
 		DbUtil dbUtil = new DbUtil();
-		List<Employee> listEmp = dbUtil.getEmployeeDao().getAll();
-		for (Employee e : listEmp) {
-			e.setDepartment(dbUtil.getDepartmentDao().getById(e.getDepartment().getId()));
+		try{
+			List<Employee> listEmp = dbUtil.getEmployeeDao().getAll();
+			for (Employee e : listEmp) {
+				e.setDepartment(dbUtil.getDepartmentDao().getById(e.getDepartment().getId()));
+			}
+			for (Employee e : listEmp) {
+				System.out.println(e.getName() + " " + e.getJobTitle() + " ( started " + e.getStartDate().toString("MMMM-dd-YYYY") + " )"
+						+ " - Department: " + e.getDepartment().toString());
+			}
 		}
-		for (Employee e : listEmp) {
-			System.out.println(e.getName() + " " + e.getJobTitle() + " ( started " + e.getStartDate().toString("MMMM-dd-YYYY") + " )"
-					+ " - Department: " + e.getDepartment().toString());
+		catch(BadSqlGrammarException e){
+			logger.error("Unable to execute query");
 		}
 		
 	}
